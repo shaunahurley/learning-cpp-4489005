bd1c9cfc8498796b984724058f0f7ec239ab9f16
shaunahurley <72588201+shaunahurley@users.noreply.github.com>
2024-06-11 21:59:38 +0000

    Finished

--- src/Ch02/02_10b/CodeDemo.cpp
+++ src/Ch02/02_10b/CodeDemo.cpp
@@ -6,7 +6,17 @@
 #include <cstdint>
 
 int main(){
-    
+    float flt;
+    int32_t sgn;
+    uint32_t unsgn;
+
+    flt = -7.66;
+    sgn = flt;
+    unsgn = sgn;
+    std::cout << "float: " << flt << std::endl;
+    std::cout << "sgn: " << sgn << std::endl;
+    std::cout << "usgn: " << (int32_t) unsgn << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
