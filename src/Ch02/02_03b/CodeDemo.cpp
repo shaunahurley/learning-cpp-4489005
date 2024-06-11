35a0b3f8fab1c66f4e359d5eae832a8ff57dd8d0
shaunahurley <72588201+shaunahurley@users.noreply.github.com>
2024-06-11 20:49:34 +0000

    Finished

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,28 @@
 
 #include <iostream>
 
+int a,b = 5; // single comment line
+
+/* Mulit-line comment
+* 
+*/
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "my_flag: " << my_flag << std::endl;
+
+    my_flag = true;
+    std::cout << "my_flag: " << my_flag << std::endl;
+    std::cout << "a+b: " << a+b << std::endl;
+    std::cout << "b-a: " << b-a << std::endl;
+
+    unsigned int positive;
+    positive = b-a;
+    std::cout << "b - a (unsigned): " << positive << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
