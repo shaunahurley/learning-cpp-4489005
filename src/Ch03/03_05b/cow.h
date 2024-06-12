6c808f8d4f3d7fbaf417339ac8d66c969ab96bf0
shaunahurley <72588201+shaunahurley@users.noreply.github.com>
2024-06-12 23:36:58 +0000

    Finished

---
+++ src/Ch03/03_05b/cow.h
@@ -0,0 +1,21 @@
+#pragma once  // not all compilers support this, we use the below more often
+
+#ifndef COW_H // if COW_H macro is not defined, include the following code
+#define COW_H
+#include <string>
+
+enum class cow_purpose {dairy, meat, hide, pet};
+
+class cow{
+public:
+    cow(std::string name_i, int age_i, cow_purpose purpose_i);
+    std::string get_name() const;
+    int get_age() const;
+    cow_purpose get_purpose() const;
+    void set_age(int new_age);
+private:
+    std::string name;
+    int age;
+    cow_purpose purpose;
+};
+#endif // COW_H
